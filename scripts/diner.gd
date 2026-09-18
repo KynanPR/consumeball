32d93348c4b37f260e2f096921dd03dff8c72523
SHUcarlton <Caloth982@googlemail.com>
2026-09-18 13:22:54 +0100

    finished diner script

--- scripts/diner.gd
+++ scripts/diner.gd
@@ -1,4 +1,15 @@
 extends Node2D
+enum desired_food {apple, pizza}
+
+var current_food = desired_food.apple
+
+func change_current_food() -> void:
+	if (current_food == desired_food.apple):
+		current_food = desired_food.pizza
+		pass
+	else: if( current_food == desired_food.pizza):
+		current_food = desired_food.apple
+		pass
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,5 +22,11 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_area_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+func _on_area_body_entered(body: Food_Item) -> bool:
+	if (body.get_food_type() == current_food):
+		return true
+		pass
+	else: if (body.get_food_type() != current_food):
+		return false
+		pass
+	return false
