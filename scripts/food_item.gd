d22b0e406afaddd508df91bfa1e6c804757f7b6e
kynanpr <kynanpr@gmail.com>
2026-09-18 12:56:01 +0100

    Basic Food

---
+++ scripts/food_item.gd
@@ -0,0 +1,20 @@
+class_name Food_Item extends RigidBody2D
+
+enum Food_Type {
+	apple,
+	pizza
+}
+
+var m_foodType: Food_Type
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func get_food_type() -> Food_Type:
+	return m_foodType
