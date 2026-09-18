0203bd6c69afc59d5060368d0f2b408117f984ac
SHUcarlton <Caloth982@googlemail.com>
2026-09-18 14:38:08 +0100

    added indicator

--- scripts/diner.gd
+++ scripts/diner.gd
@@ -1,14 +1,18 @@
 extends Node2D
 enum desired_food {apple, pizza}
 
-var current_food = desired_food.pizza
+var current_food = desired_food.apple
+@onready var indicator: Sprite2D = $"area/Current food sprite"
+
 
 func change_current_food() -> void:
 	if (current_food == desired_food.apple):
 		current_food = desired_food.pizza
+		indicator.region_rect.position.y = 64
 		pass
 	else: if( current_food == desired_food.pizza):
 		current_food = desired_food.apple
+		indicator.region_rect.position.y = 0
 		pass
 
 
@@ -23,9 +27,9 @@ func _process(delta: float) -> void:
 
 
 func _on_area_body_entered(body: Food_Item) -> bool:
-	print("here")
 	if (body.get_food_type() == current_food):
 		body.queue_free()
+		change_current_food()
 		return true
 		pass
 	else:
